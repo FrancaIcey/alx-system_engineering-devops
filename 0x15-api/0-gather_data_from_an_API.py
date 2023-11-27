7b6be64b5c889077a7603ecfe1d33648a01443f0
Francaicey <francawhyte447@gmail.com>
2023-11-27 13:13:25 -0800

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""
+Fetch and display employee TODO list progress using a REST API.
+"""
+
 import requests
 import sys
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+def get_employee_todo_progress(employee_id):
+    try:
+        user_data = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}").json()
+        todos_data = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}").json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+        completed_tasks = [task for task in todos_data if task["completed"]]
+        num_done_tasks, total_tasks = len(completed_tasks), len(todos_data)
+
+        print(f"Employee {user_data['name']} is done with tasks ({num_done_tasks}/{total_tasks}):")
+        print(f"{user_data['name']}:", *([f"\n\t{task['title']}" for task in completed_tasks] or [" No completed tasks."]))
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./todo_script.py <employee_id>")
+        sys.exit(1)
+
+    get_employee_todo_progress(int(sys.argv[1]))
