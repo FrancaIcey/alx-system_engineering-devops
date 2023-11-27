1f618d4bc554a860192b06bfb65e100e617d6a15
Francaicey <francawhyte447@gmail.com>
2023-11-27 13:17:42 -0800

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
 """
-Fetch and display employee TODO list progress using a REST API.
+Uses the JSON placeholder api to query data about an employee
 """
 
-import requests
-import sys
+from requests import get
+from sys import argv
 
-def get_employee_todo_progress(employee_id):
-    try:
-        user_data = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}").json()
-        todos_data = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}").json()
+if __name__ == '__main__':
+    main_url = 'https://jsonplaceholder.typicode.com'
+    todo_url = main_url + "/user/{}/todos".format(argv[1])
+    name_url = main_url + "/users/{}".format(argv[1])
+    todo_result = get(todo_url).json()
+    name_result = get(name_url).json()
 
-        completed_tasks = [task for task in todos_data if task["completed"]]
-        num_done_tasks, total_tasks = len(completed_tasks), len(todos_data)
-
-        print(f"Employee {user_data['name']} is done with tasks ({num_done_tasks}/{total_tasks}):")
-        print(f"{user_data['name']}:", *([f"\n\t{task['title']}" for task in completed_tasks] or [" No completed tasks."]))
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        sys.exit(1)
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./todo_script.py <employee_id>")
-        sys.exit(1)
-
-    get_employee_todo_progress(int(sys.argv[1]))
+    todo_num = len(todo_result)
+    todo_complete = len([todo for todo in todo_result
+                         if todo.get("completed")])
+    name = name_result.get("name")
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, todo_complete, todo_num))
+    for todo in todo_result:
+        if (todo.get("completed")):
+            print("\t {}".format(todo.get("title")))
